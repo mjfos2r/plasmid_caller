173b5d067b1b8ac7b0d1053603dc9aa727a953af
Michael Foster <50351736+mjfos2r@users.noreply.github.com>
2025-05-19 16:58:27 -0400

    bad build hook.
    Signed-off-by: Michael Foster <50351736+mjfos2r@users.noreply.github.com>

--- scripts/build_hooks.py
+++ scripts/build_hooks.py
@@ -4,32 +4,43 @@ import os
 import stat
 import subprocess
 from pathlib import Path
+import shutil
+from typing import Any, Dict
 
-def post_build(directory):
-    """run after package build to ensure proper permissions for our scripts"""
-    script_path = Path(directory) / "scripts" / "manage_blast.sh"
-    # make it executable in case it isn't
-    if script_path.exists():
-        current_mode = os.stat(script_path).st_mode
-        os.chmod(script_path, current_mode | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
+from hatchling.builders.hooks.interface import BuildHookInterface
 
-    # make sure vendors dir exists.
-    vendor_dir = Path(directory) / "vendor" / "blast"
-    vendor_dir.mkdir(parents=True, exist_ok=True)
+class CustomBuildHook(BuildHookInterface):
+    """
+    custon build hook to handle blast installation and database setup.
+    """
+    def initialize(self, version: str, build_data: Dict[str, Any]) -> None:
+        """init hook"""
+        pass
 
-    # check for blast and install if we don't have it.
-    try:
-        subprocess.run(
-            [str(script_path), "path"],
-            check=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE
-        )
-        print("BLAST is available")
-    except subprocess.CalledProcessError:
-        print("Failed to find BLAST. Installation required at runtime")
-    except Exception as E:
-        print(f"Error checking BLAST: {e}")
+    def finalize(self, version: str, build_data: Dict[str, Any], artifact_path: str) -> None:
+        """run after building the package to ensure script permissions and DB setup."""
+        directory = Path(build_data["directory"])
+        script_path = directory / "scripts" / "manage_blast.sh"
+        if script_path.exists():
+            # make it executable in case it isn't
+            current_mode = os.stat(script_path).st_mode
+            os.chmod(script_path, current_mode | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
 
-    return True
+        # make sure vendors dir exists.
+        vendor_dir = Path(directory) / "vendor" / "blast"
+        vendor_dir.mkdir(parents=True, exist_ok=True)
 
+        # check for blast and install if we don't have it.
+        try:
+            subprocess.run(
+                [str(script_path), "path"],
+                check=True,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE
+            )
+            print("BLAST is available")
+        except subprocess.CalledProcessError:
+            print("Failed to find BLAST. Installation required.")
+            subprocess.run([str(script_path), "install"], check=False)
+        except Exception as E:
+            print(f"Error checking BLAST: {e}")
