cb5b5ca866c6b9c7cd7f0ae3d2121e2243b50907
Michael J. Foster <50351736+mjfos2r@users.noreply.github.com>
2025-05-19 17:04:13 +0000

    fixed?

--- v3/new_code/make_dbs/make_db.py
+++
@@ -1,8 +0,0 @@
-if RUN_DB_BOOL is True:
-    make_db_cmd = "makeblastdb -in "+fasta_for_db+" -out "+blast_db+" -dbtype 'nucl' -hash_index"
-    print(make_db_cmd)
-    subprocess.run(make_db_cmd, shell=True)
-
-else:
-    print("Blast DB already exists! Not creating it again!")
-    print("Change RUN_DB_BOOL to True if you need to rebuild the DB.")
