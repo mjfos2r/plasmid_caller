8173401497c4d45bc3d4be1a4e9197e4a673af9f
Michael Foster <50351736+mjfos2r@users.noreply.github.com>
2025-05-20 16:01:45 -0400

    silly typo
    Signed-off-by: Michael Foster <50351736+mjfos2r@users.noreply.github.com>

--- src/plasmid_caller/main.py
+++ src/plasmid_caller/main.py
@@ -531,7 +531,7 @@ def main(args=None):
         db_name = Path(db_path).stem
         results_dir = output_path / db_name / "xml_files"
         tables_dir = output_path / db_name / "tables"
-        results_dir.mkdirs(parents=True, exist_ok=True)
+        results_dir.mkdir(parents=True, exist_ok=True)
         tables_dir.mkdir(parents=True, exist_ok=True)
 
         if not args.skip_blast:
